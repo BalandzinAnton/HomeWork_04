dd8ab985ff568d8509a185979e44b27725ea4672
Anton <balandzin2602@gmail.com>
2022-09-26 18:12:07 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B с использованием цикла.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using static System.Console;
+Clear();
+Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int Power = GetPower(A,B);
+WriteLine(Power);
+
+
+int GetPower(int num1, int num2)
+{   
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+    result = result*num1; 
+    }
+    return result;
+}
